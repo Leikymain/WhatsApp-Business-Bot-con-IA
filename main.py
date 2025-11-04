4d3f6a0dbce080efac7d0dbfe48b1cdb5278e022
Leikymain <lagojorge24@gmail.com>
2025-11-04 19:57:56 +0100

    Fix main

--- main.py
+++ main.py
@@ -22,7 +22,7 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],  # o tu dominio
+    allow_origins=["whatsapp-business-bot-demo.up.railway.app"],  # o tu dominio
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
